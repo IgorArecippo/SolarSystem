41d5cb4b5c10be6fa03e74528e2b0a6f1b5adc2c
Igor Arecippo <iarecippo@gmail.com>
2022-08-18 18:32:24 -0300

    finalizando os requisitos

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {
+          missions.map((m) => (
+            <MissionCard
+              name={ m.name }
+              year={ m.year }
+              country={ m.country }
+              destination={ m.destination }
+              key={ m.name }
+            />
+          ))
+        }
       </div>
     );
   }
